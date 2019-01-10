d2b6a7532d635264195ff2fb6aa0c91ced3d4c82
guozhijie <gzj1000xp@hotmail.com>
2019-01-10 15:57:29 +0800

    ec2

--- ec2_describe-instances.py
+++ ec2_describe-instances.py
@@ -1,7 +1,18 @@
 import boto3
+import json
+
+Filters = [
+    {
+        'Name': 'tag:Env',
+        'Values': ['test']
+    }
+]
+InstanceIds = []
+Dryrun = True
 
 
 ec2 = boto3.client('ec2')
-response = ec2.describe_instances()
+response = ec2.describe_instances(Filters=Filters)
 
-print(response)
+# print(response)
+print(json.dumps(response, indent=4, sort_keys=True, default=str))
